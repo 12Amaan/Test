e57604321f83d74d89c2029306c4bfba347f02d9
AmaanBH <95271384+AmaanBH@users.noreply.github.com>
2022-05-22 09:43:58 +0530

    Adding Discovery Eureka Service

---
+++ DiscoverService/src/test/java/com/discoveryService/DiscoverService/DiscoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discoveryService.DiscoverService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
